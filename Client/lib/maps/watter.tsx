12aabc8184cd26bdaed9e779a7888aa8ba129604
Darlan Andrade <flame2br@gmail.com>
2020-10-14 20:25:16 -0300

    finalizando desenho do tabuleiro

--- Client/lib/maps/watter.tsx
+++ Client/lib/maps/watter.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="Ocean_SpriteSheet" tilewidth="32" tileheight="32" tilecount="16" columns="8">
- <image source="../Documents/DBG/Ocean_SpriteSheet.png" width="256" height="64"/>
+ <image source="../assets/ocean.png" width="256" height="64"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="100"/>
