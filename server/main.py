9f808f1b286f633320104aa5d7377f2e6962b3f1
Danilo Sambugaro <dan.sambugaro@gmail.com>
2020-10-23 16:06:07 -0300

    Implementando main class

--- server/main.py
+++ server/main.py
@@ -1,8 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from services import player
+import logging as log
+from time import sleep
+
+from services import player, match, ranking
+
+
+class Main:
+    services_poll = []
+
+    def __init__(self, log_level=log.INFO):
+        log.basicConfig(
+            format='[ Servidor ][ %(levelname)s ] %(message)s', level=log_level)
+
+    def start(self):
+        log.info('Iniciando servidor')
+        self.services_poll = [player, match, ranking]
+        log.info('Criando instancia dos serviços')
+        self.services_poll = [service.Manager()
+                              for service in self.services_poll]
+        log.info('Iniciando serviços')
+        for service in self.services_poll:
+            service.start()
+        log.info('Servidor iniciado, Ctrl + C para parar a execução ...')
+
+    def join(self):
+        for service in self.services_poll:
+            service.join()
+
+    def stop(self):
+        log.info('Parando execução do servidor')
+        for service in self.services_poll:
+            service.stop()
+        log.info('Até logo e obrigado pelos peixes!')
+
 
 if __name__ == "__main__":
-    player_service = player.Manager()
-    player_service.start()
+    server = Main()
+    server.start()
+    try:
+        while True:
+            sleep(5)
+    except KeyboardInterrupt:
+        server.stop()
