fd0ce631058c512e97004a95177a02e44ece0bdf
Danilo Sambugaro <dan.sambugaro@gmail.com>
2020-10-23 16:04:39 -0300

    Abstraindo métodos de comunicação

--- server/services/connector.py
+++ server/services/connector.py
@@ -6,6 +6,7 @@ import paho.mqtt.client as mqtt
 
 class Connector:
 
+    encoding = 'utf-8'
     host = 'mqtt.eclipse.org'
     port = 1883
     qos = 1
@@ -15,16 +16,28 @@ class Connector:
         self.topic = topic
         self.client = mqtt.Client()
 
-    def start(self):
-        self.running = True
-        self.run()
-
     def stop(self):
         self.running = False
         self.client.disconnect()
+
+    def ping(self):
+        self.client.publish('/{}/pong'.format(self.topic),
+                            'pong'.encode(self.encoding), self.qos)
 
     def publish(self, topic, value):
         self.client.publish(topic, value, self.qos)
 
     def connect(self):
         self.client.connect(self.host, self.port)
+
+    def on_connect(self, client, userdata, flags, rc):
+        self.client.subscribe('/DBG/{}/+'.format(self.topic))
+
+    def setup(self):
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+    def run(self):
+        self.setup()
+        self.connect()
+        self.client.loop_forever()
--- server/services/match/manager.py
+++ server/services/match/manager.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging as log
+from threading import Thread
+
 from ..connector import Connector
 from .processor import Processor
 
 
-class Manager(Connector):
+class Manager(Connector, Thread):
 
-    def __init__(self):
-        super(Manager, self).__init__('match-service')
+    def __init__(self, log_level=log.INFO):
+        super(Manager, self).__init__('match')
+        log.basicConfig(
+            format='[ Match Service ][ %(levelname)s ] %(message)s', level=log_level)
 
-    def on_connect(self, client, userdata, flags, rc):
-        self.client.subscribe('/{}/+'.format(self.topic))
+    def processor(self, event, data):
+        return Processor(self, event, data)
 
     def on_message(self, client, userdata, msg):
         event = msg.topic.split('/')[-1]
-        data = msg.payload.decode('utf-8')
-        handler = Processor(event, data)
+        data = msg.payload.decode(self.encoding)
+        handler = self.processor(event, data)
         handler.start()
-
-    def setup(self):
-        self.client.on_connect = self.on_connect
-        self.client.on_message = self.on_message
-
-    def run(self):
-        self.setup()
-        self.connect()
-        self.client.loop_forever()
