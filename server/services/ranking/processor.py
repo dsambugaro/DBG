e225dc61bdadae2d666504f0cd10840ff4cbda61
Danilo Sambugaro <dan.sambugaro@gmail.com>
2020-11-02 09:48:29 -0300

    Iniciando implementação das funções de ranking

--- server/services/ranking/processor.py
+++ server/services/ranking/processor.py
@@ -4,13 +4,23 @@
 from threading import Thread
 
 from ..handler import Handler
+from ..database import Database
 
 
 class Processor(Handler, Thread):
     def __init__(self, manager, event, data):
         super(Processor, self).__init__(event, data)
         self.manager = manager
+        self.db = Database('rank')
 
     def ping(self, data):
         if data == 'ping':
             self.manager.ping()
+    
+    def register(self, data):
+        pass
+
+    def query(self, data):
+        pass
+
+
