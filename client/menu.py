df9b46fb66bb2f775d17d8c65facac3f817adec8
Danilo Sambugaro <dan.sambugaro@gmail.com>
2020-11-02 11:00:28 -0300

    Ajustando chamada de funções

--- client/menu.py
+++ client/menu.py
@@ -3,6 +3,7 @@
 
 from utils import Utils
 
+
 class menu:
 
     def print_ship_art(self):
@@ -12,39 +13,31 @@ class menu:
             '                                  )___(\n'
             '                           _______/__/_\n'
             '                  ___     /===========|   ___\n'
-            ' ____       __   [\\\]___/____________|__[///]   __\n'
-            ' \   \_____[\\]__/___________________________\__[//]___\n'
-            '  \                                                    |\n'
-            '   \                                                  /\n'
+            ' ____       __   [\\\\\\]___/____________|__[///]   __\n'
+            ' \\   \\_____[\\\\]__/___________________________\\__[//]___\n'
+            '  \\                                                    |\n'
+            '   \\                                                  /\n'
             '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
             '* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n'
         )
 
-    def register_menu(self):
-        print_ship_art()
-        print('* NOVO JOGADOR *\n')
-        username = input('USERNAME: ')
-        senha = input('SENHA: ')
-        # New User Function
-        logged_in_menu()
-
     def config_menu(self):
-        #Settings Function
-        print_ship_art()
+        # Settings Function
+        self.print_ship_art()
         print('* CONFIGURACOES *\n')
         print('MODO PRETTY\n')
         print('PORT/IP\n')
         print('SAIR\n')
 
         while True:
-            command = input('--> ')
+            command = input('--> ').strip()
             if 'sair' == command.lower():
                 # Exit Game
                 break
 
             elif 'modo pretty' == command.lower():
                 print('* ATIVAR MODO PRETTY? (TABULEIRO COM EMOJIS) *\n')
-                command = input('Y/N --> ')
+                command = input('Y/N --> ').strip()
                 if 'y' == command.lower():
                     # Activate pretty mode
                     print('* MODO PRETTY ATIVADO *\n')
@@ -59,79 +52,78 @@ class menu:
             else:
                 # Invalid Command
                 print('\nComando inválido\n')
-    
+
     def logged_in_menu(self):
-        print_ship_art()
+        self.print_ship_art()
         print('BUSCAR JOGADOR\n')
         print('ENCONTRAR PARTIDA\n')
         print('CONFIGURACOES')
         print('SAIR\n')
 
         while True:
-            command = input('--> ')
+            command = input('--> ').strip()
             if 'sair' == command.lower():
                 # Exit Game
-                break  
+                break
 
             elif 'buscar jogador' == command.lower():
-                #Finding player profile
-                print_ship_art()
+                # Finding player profile
+                self.print_ship_art()
                 print('* NOME DO JOGADOR *\n')
-                command = input('--> ')
-                
+                command = input('--> ').strip()
 
-            elif 'encontrar partida'== command.lower():
-                #Matchmaking Function
-                print_ship_art()
+            elif 'encontrar partida' == command.lower():
+                # Matchmaking Function
+                self.print_ship_art()
                 print('* ENCONTRANDO PARTIDA *\n')
 
-            elif 'configuracoes'== command.lower():
-                config_menu()
-                
+            elif 'configuracoes' == command.lower():
+                self.config_menu()
+
             else:
                 # Invalid Command
                 print('\nComando inválido\n')
 
     def login_menu(self):
-        print_ship_art()
+        self.print_ship_art()
         print('* JOGADOR EXISTENTE *\n')
-        username = input('USERNAME: ')
-        senha = input('SENHA: ')
+        self.username = input('USERNAME: ').strip()
+        self.senha = input('SENHA: ').strip()
 
         if 'USERNAME' == True and 'SENHA' == True:
             # Database Verification
             print('* LOG SUCCESS *\n')
-            logged_in_menu()
+            self.logged_in_menu()
         else:
             print('* LOG FAILED *')
 
     def register_menu(self):
-        print_ship_art()
+        self.print_ship_art()
         print('* NOVO JOGADOR *\n')
-        username = input('USERNAME: ')
-        senha = input('SENHA: ')
+        self.username = input('USERNAME: ').strip()
+        self.senha = input('SENHA: ').strip()
         # New User Function
-        logged_in_menu()
+        self.logged_in_menu()
 
     def main_menu(self):
-        print_ship_art()
+        self.print_ship_art()
         print('REGISTRAR\n')
         print('LOGAR\n')
         print('SAIR\n')
-        
+
         while True:
-            command = input('--> ')
+            command = input('--> ').strip()
             if 'sair' == command.lower():
                 # Exit Game
-                break  
+                break
 
             elif 'registrar' == command.lower():
-                #Register new user
-                register_menu()
+                # Register new user
+                self.register_menu()
 
-            elif 'logar'== command.lower():
-                #Log into account
-                logged_in_menu()
+            elif 'logar' == command.lower():
+                # Log into account
+                self.logged_in_menu()
 
             else:
                 # Invalid Command
