44d87d762a65813e6bffc9bb9310b2e2e5eb8079
Danilo Sambugaro <dan.sambugaro@gmail.com>
2020-11-02 11:02:07 -0300

    Implementando fila de partida

--- server/services/match/manager.py
+++ server/services/match/manager.py
@@ -3,6 +3,7 @@
 
 import logging as log
 from threading import Thread
+from itertools import combinations
 
 from ..connector import Connector
 from .processor import Processor
@@ -10,16 +11,39 @@ from .processor import Processor
 
 class Manager(Connector, Thread):
 
+    queue = []
+
     def __init__(self, log_level=log.INFO):
         super(Manager, self).__init__('match')
         log.basicConfig(
-            format='[ Match Service ][ %(levelname)s ] %(message)s', level=log_level)
+            format='[ Match Service ][ %(levelname)s ] %(message)s',
+            level=log_level
+        )
 
     def processor(self, event, data):
         return Processor(self, event, data)
 
-    def on_message(self, client, userdata, msg):
-        event = msg.topic.split('/')[-1]
-        data = msg.payload.decode(self.encoding)
-        handler = self.processor(event, data)
-        handler.start()
+    def handler(self, event, data):
+        if event == 'find_game':
+            if data not in self.queue:
+                self.queue.append(data)
+            while len(self.queue) >= 2:
+                combinations_list = list(combinations(self.queue, 2))
+                players = {
+                    'one': combinations_list[0][0],
+                    'two': combinations_list[0][1],
+                }
+                try:
+                    self.queue.remove(players['one'])
+                    self.queue.remove(players['two'])
+                    new_match = self.processor('new_match', players)
+                    new_match.start()
+                except ValueError:
+                    log.error(
+                        'O valor ({}|{}) não existe na fila de partida'.format(
+                            players['one'], players['two']
+                        )
+                    )
+        else:
+            handler = self.processor(event, data)
+            handler.start()
--- server/services/match/processor.py
+++ server/services/match/processor.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from threading import Thread
+from random import getrandbits
 
 from ..handler import Handler
 
@@ -14,3 +15,23 @@ class Processor(Handler, Thread):
     def ping(self, data):
         if data == 'ping':
             self.manager.ping()
+
+    def new_match(self, data):
+        player_one = data['one']
+        player_two = data['two']
+
+        response_one = {
+            'code': 42,
+            'msg': 'match found',
+            'adversary': player_two,
+            'goes_first': bool(getrandbits(1))
+        }
+
+        response_two = response_one.copy()
+        response_two['adversary'] = player_one
+        response_two['goes_first'] = not response_one['goes_first']
+
+        self.manager.publish_event(
+            player_one['clientUUID'], response_one)
+        self.manager.publish_event(
+            player_two['clientUUID'], response_two)
